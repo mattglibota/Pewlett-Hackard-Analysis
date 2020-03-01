a5b70ba938fd7711d9dff84330fbfdeaf09d1216
mattglibota <59847215+mattglibota@users.noreply.github.com>
2020-03-01 13:52:13 -0800

    updated challenge

--- Queries/challenges.sql
+++ Queries/challenges.sql
@@ -23,3 +23,31 @@ FROM
 	) tmp WHERE rn = 1
 GROUP BY title 
 ORDER BY count desc
+
+-- who's ready for a mentor?
+SELECT
+e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+--INTO mentor
+FROM
+employees e
+INNER JOIN titles t ON e.emp_no = t.emp_no
+INNER JOIN dept_employee de ON e.emp_no = de.emp_no
+WHERE t.to_date = ('9999-01-01')
+AND de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+
+--count mentor titles to check how many matching
+SELECT count(title), title
+INTO mentor_titles_count
+FROM
+	(SELECT *, ROW_NUMBER() OVER 
+		(PARTITION BY (emp_no) ORDER BY from_date DESC) rn
+	FROM mentor
+	) tmp WHERE rn = 1
+GROUP BY title 
+ORDER BY count desc
