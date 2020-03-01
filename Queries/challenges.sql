238b0faa7a6889e2ba0bc0ff4a5e09acb0cca349
mattglibota <59847215+mattglibota@users.noreply.github.com>
2020-03-01 14:11:27 -0800

    final challenge update

--- Queries/challenges.sql
+++ Queries/challenges.sql
@@ -6,12 +6,13 @@ e.last_name,
 t.title,
 t.from_date,
 s.salary
-INTO titles_retiring
+--INTO titles_retiring
 FROM employees e
 INNER JOIN titles t ON e.emp_no = t.emp_no
 INNER JOIN salaries s ON e.emp_no = s.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (t.to_date = '9999-01-01');
 
 --only the most recent titles
 SELECT count(title), title
